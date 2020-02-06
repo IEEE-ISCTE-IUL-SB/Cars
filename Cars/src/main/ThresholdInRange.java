3d0cbb96398be6f7ae5ef5b0cdfa8ef19f82904e
IEEE-ACM <IEEE-ACM@DESKTOP-TU6S9NF>
2020-02-06 14:47:15 +0000

    test

--- Cars/src/main/ThresholdInRange.java
+++ Cars/src/main/ThresholdInRange.java
@@ -51,7 +51,6 @@ public class ThresholdInRange {
     private JLabel imgDetectionLabel;
     private CaptureTask captureTask;
   
-    //ygiygfifgifii
     
     public ThresholdInRange(String[] args) {
         int cameraDevice = 0;
@@ -260,10 +259,9 @@ public class ThresholdInRange {
         frame.repaint();
     }
     public static void main(String[] args) {
-        // Load the native OpenCV library
+    	
         System.loadLibrary(Core.NATIVE_LIBRARY_NAME);
-        // Schedule a job for the event dispatch thread:
-        // creating and showing this application's GUI.
+        
         javax.swing.SwingUtilities.invokeLater(new Runnable() {
             @Override
             public void run() {
